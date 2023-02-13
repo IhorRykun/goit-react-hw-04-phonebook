90260a519943087852d24a709c54d6eb4c5d24f5
Ihor <ihorrykun@gmail.com>
2023-02-13 20:48:26 +0200

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import css from '../App/App.module.css';
-import { ContactsForm } from '../components/ContactForm/ContactForm';
+import { ContactsForm } from 'components/ContactForm/ContactForm';
 import { Filter } from '../components/Filter/Filter';
 import { Contacts } from '../components/Contacts/Contacts';
 
